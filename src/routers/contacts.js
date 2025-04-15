895d638c8e3a96feb998a91d15cfc6bfb29fa8ed
veronikavatsyak <veronikateslya@ukr.net>
2025-04-14 22:04:40 -0400

    correcting get request

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,7 +12,7 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contact', ctrlWrapper(addContactController));
+router.post('/contacts', ctrlWrapper(addContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 export default router;
